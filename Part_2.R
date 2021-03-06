f674bfebdebbc6d123fe7b3350df9e3963e1d7f3
Abbas212 <aviranimahdiabbas212@gmail.com>
2021-03-06 21:49:25 +0530

    Part 2 Done...

---
+++ Part_2.R
@@ -0,0 +1,20 @@
+directory=paste(getwd(), "/", "Air", sep="")
+results <- data.frame(id=numeric(0), nobs=numeric(0))
+complete <- function(directory, id = 1)
+{
+        for (i in id)
+        {
+                if (i<10)
+                {path = paste(directory, "/",  "00" , i, ".csv", sep="")}
+                if (i>9 && i<100)
+                {path = paste(directory, "/",  "0" , i, ".csv", sep="")}
+                if(i>=100)
+                {path = paste(directory, "/" , i, ".csv", sep="")}
+                total_data = read.csv(path)
+                interested_data = total_data[(!is.na(total_data$sulfate)), ]
+                interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+                nobs <- nrow(interested_data)
+                results <- rbind(results, data.frame(id=i, nobs=nobs))
+        }
+        results
+}
